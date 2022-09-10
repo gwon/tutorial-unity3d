e5a64d56daad6020396b2451fe9390cc75345f2b
gwon <great.gwon@gmail.com>
2022-09-10 14:33:15 +0700

    keyboard input

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject dino;
+    public float speed = 10;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            Vector3 next = dino.transform.position;
+            next.x -= speed * Time.deltaTime;
+            dino.transform.position = next;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Vector3 next = dino.transform.position;
+            next.x += speed * Time.deltaTime;
+            dino.transform.position = next;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            Vector3 next = dino.transform.position;
+            next.y -= speed * Time.deltaTime;
+            dino.transform.position = next;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            Vector3 next = dino.transform.position;
+            next.y += speed * Time.deltaTime;
+            dino.transform.position = next;
+        }
+    }
+}
