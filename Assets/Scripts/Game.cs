8728dc7841ab282c8997c85b32d82c33fdeb4627
gwon <great.gwon@gmail.com>
2022-09-11 16:36:33 +0700

    UI Event

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,7 +6,7 @@ using UnityEngine.InputSystem;
 using System;
 using UnityEngine.SceneManagement;
 
-public class Game : MonoBehaviour, Controller.IPlayerActions
+public class Game : MonoBehaviour, Controller.IPlayerActions, Controller.IUIActions
 {
     public GameObject dino;
     public float speed = 10;
@@ -22,6 +22,9 @@ public class Game : MonoBehaviour, Controller.IPlayerActions
         var ui = GetComponentInChildren<UIDocument>(true).rootVisualElement;
         var btExit = ui.Q<Button>("Exit");
         btExit.clicked += OnExitClick;
+
+        controller.UI.Enable();
+        controller.UI.SetCallbacks(this);
     }
 
     private void OnExitClick()
@@ -57,5 +60,65 @@ public class Game : MonoBehaviour, Controller.IPlayerActions
             scale.x = dir.x;
             dino.transform.localScale = scale;
         }
+
+    }
+
+    void Controller.IUIActions.OnNavigate(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnNavigate");
+
+    }
+
+    void Controller.IUIActions.OnSubmit(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnSubmit");
+
+    }
+
+    void Controller.IUIActions.OnCancel(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnCancel");
+    }
+
+    void Controller.IUIActions.OnPoint(InputAction.CallbackContext context)
+    {
+        var pos = context.ReadValue<Vector2>();
+        Debug.Log($"position >>> {pos.x}, {pos.y}");
+    }
+
+    void Controller.IUIActions.OnClick(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnClick");
+
+    }
+
+    void Controller.IUIActions.OnScrollWheel(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnScrollWheel");
+
+    }
+
+    void Controller.IUIActions.OnMiddleClick(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnMiddleClick");
+
+    }
+
+    void Controller.IUIActions.OnRightClick(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnRightClick");
+
+    }
+
+    void Controller.IUIActions.OnTrackedDevicePosition(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnTrackedDevicePosition");
+
+    }
+
+    void Controller.IUIActions.OnTrackedDeviceOrientation(InputAction.CallbackContext context)
+    {
+        Debug.Log($"OnTrackedDeviceOrientation");
+
     }
 }
