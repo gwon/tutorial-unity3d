8a78ebd191e2aa4bc8d0b6851a84805818045cfd
gwon <great.gwon@gmail.com>
2022-09-11 09:49:47 +0700

    unity new input system

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,52 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Main : MonoBehaviour
 {
     public GameObject dino;
     public float speed = 10;
+    public Vector3 dir = Vector3.zero;
     void Start()
     {
 
     }
 
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnFire");
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("On move >>> " + dir);
+        dir = context.ReadValue<Vector2>();
+    }
+
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            Vector3 next = dino.transform.position;
-            next.x -= speed * Time.deltaTime;
-            dino.transform.position = next;
-
-            Vector3 scale = dino.transform.localScale;
-            scale.x = -1;
-            dino.transform.localScale = scale;
-        }
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            Vector3 next = dino.transform.position;
-            next.x += speed * Time.deltaTime;
-            dino.transform.position = next;
-
-            Vector3 scale = dino.transform.localScale;
-            scale.x = 1;
-            dino.transform.localScale = scale;
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            Vector3 next = dino.transform.position;
-            next.y -= speed * Time.deltaTime;
-            dino.transform.position = next;
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            Vector3 next = dino.transform.position;
-            next.y += speed * Time.deltaTime;
-            dino.transform.position = next;
-        }
+        Vector3 next = dino.transform.position + dir * Time.deltaTime * speed;
+        dino.transform.position = next;
     }
 }
